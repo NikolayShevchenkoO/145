3c89d52b961512d01b66d446d86d7f35d0874072
Nikolay Shevchenko <f4stnikolay@yandex.ru>
2022-03-24 11:37:32 +0700

    all is gud

---
+++ src/main/java/ru/javarush/shevchenko/cryptoanalizer/Commands/FillDictionary.java
@@ -0,0 +1,29 @@
+package ru.javarush.shevchenko.cryptoanalizer.Commands;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Set;
+
+public class FillDictionary {
+    public static Set<String> filDictonary(String firstDictonary) {
+        Set<String> dict = new HashSet<>() ;
+        var halfRslt = FileRead.fileReader(firstDictonary);
+        var Rslt = halfRslt.toLowerCase();
+        var resultSet = Rslt.split("[^а-яё]+"); //Очистка слов дописать
+        for (String s : resultSet) {
+            if (s.length()>2){
+                dict.add(s);
+               // System.out.println(s);
+            }
+
+
+        }
+//        FileWrit.fileWriter("halfRslt.txt", halfRslt);
+//        FileWrit.fileWriter("Rslt.txt", Rslt);
+//        FileWrit.fileWriter("resSet.txt", String.valueOf(resultSet));
+//
+//        FileWrit.fileWriter("dist.txt", String.valueOf(dict));
+
+        return dict;
+    }
+}
